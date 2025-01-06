b2a158402b760382827777f7e5101517c0114b0a
Daniel Kiss <littlesnorrboy@gmail.com>
2025-01-06 15:53:08 +0100

    Remove the welcome h1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,6 @@
     let phiRads = $derived(phi * Math.PI / 180.0)
 </script>
 
-<h1>Welcome</h1>
-
 <RotateCanvas imagePath="rocket.jpg" angle={phiRads} />
 <input type="number" min="0" max="360" step="5" bind:value={phi} />
 <input name="angle" type="range" min="0" max="360" step="5" bind:value={phi} />
